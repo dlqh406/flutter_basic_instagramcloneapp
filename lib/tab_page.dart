acd945ca6c8c222c1c50271f2a571131bdea1e55
Lee bosung <dlqh406@gmail.com>
2020-07-26 07:42:41 +0900

    tab_page.dart : 탭 클릭에 따른 상태변화
    onTap: _onItemTapped, : 클릭되어있는 index를 int로 반환 한다 // 아래 함수로 클릭index 활성화
    currentIndex: _selectedIndex : 현재 활성화 되어있는 탭(파란불 들어옴)
    
     void _onItemTapped(int index) {
        setState(() {
          _selectedIndex = index;
        });
      }
    : 아래 탭 클릭과 동시에 body화면이 바뀜과 동시에 활성화탭이 바뀜(파란불 들어옴)

--- lib/tab_page.dart
+++ lib/tab_page.dart
@@ -7,10 +7,16 @@ class TabPage extends StatefulWidget {
 
 class _TabPageState extends State<TabPage> {
   int _selectedIndex =  0;
+  List _pages = [
+    Text('page1'),
+    Text('page2'),
+    Text('page3'),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Center( child: _pages[_selectedIndex],),
       bottomNavigationBar: BottomNavigationBar(
         onTap: _onItemTapped,
         currentIndex: _selectedIndex,
@@ -18,13 +24,10 @@ class _TabPageState extends State<TabPage> {
           BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("Home")),
           BottomNavigationBarItem(icon: Icon(Icons.search), title: Text("Search")),
           BottomNavigationBarItem(icon: Icon(Icons.account_circle), title: Text("Account")),
-
         ]
-
         ,),
     );
   }
-
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
