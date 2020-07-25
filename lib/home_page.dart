a02607f42ba993e2dc578dab505ce8fd0fb41286
Lee bosung <dlqh406@gmail.com>
2020-07-26 08:46:04 +0900

    Home_page.dart : 홈화면 총구성
    child: Column(
                children: <Widget>[ ] : 복수의 위젯을 배열안에다  구성( row는 옆방향으로 )
    
    Padding(padding: EdgeInsets.all(16.0)) 패딩 넣는법 : 배열안에 넣으면됨
    
    child: CircleAvatar(
                              backgroundImage: NetworkImage('https://s-i.huffpost.com/gen/3948866/thumbs/o-PEPE-THE-FROG-570.jpg?3'),
                            )
    : 동그란 원안에 이미지 넣는법
    
    SizedBox : 박스 크기를 조정 / 커서 앞에두고 option+엔터하면 자동 wrap됨
    
     elevation: 4.0  : 그림자 깊이

---
+++ lib/home_page.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Instagram Clone",
+          style:TextStyle(color: Colors.black,
+             fontWeight: FontWeight.bold),),
+      ),
+      body:_buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: SafeArea(
+        child: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text("인스타에 오신걸 환영합니다", style: TextStyle(fontSize: 24.0)),
+              Padding(padding: EdgeInsets.all(8.0)),
+              Text("사진과 동영상을 보려면 팔로우 하세요"),
+              Padding(padding: EdgeInsets.all(16.0)),
+              SizedBox(
+                width: 260.0,
+                child: Card(
+                  elevation: 4.0 ,
+                  child: Column(
+                    children: <Widget>[
+                      Padding(padding: EdgeInsets.all(10.0)),
+                      SizedBox(
+                        width: 80.0,
+                        height: 80.0,
+                        child: CircleAvatar(
+                          backgroundImage: NetworkImage('https://s-i.huffpost.com/gen/3948866/thumbs/o-PEPE-THE-FROG-570.jpg?3'),
+                        ),
+                      ),
+                      Padding(padding: EdgeInsets.all(10.0)),
+                      Text('이메일 주소', style: TextStyle(fontWeight: FontWeight.bold),),
+                      Text('이름'),
+                      Padding(padding: EdgeInsets.all(8.0)),
+                      SizedBox(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            SizedBox(
+                              width: 70,
+                              height: 70,
+                              child: Image.network('https://s-i.huffpost.com/gen/3948866/thumbs/o-PEPE-THE-FROG-570.jpg?3', fit: BoxFit.cover)
+                        ),
+                            Padding(padding: EdgeInsets.all(2.0)),
+                            SizedBox(
+                                width: 70,
+                                height: 70,
+                                child: Image.network('https://s-i.huffpost.com/gen/3948866/thumbs/o-PEPE-THE-FROG-570.jpg?3',fit: BoxFit.cover)
+                            ),
+                            Padding(padding: EdgeInsets.all(2.0)),
+                            SizedBox(
+                                width: 70,
+                                height: 70,
+                                child: Image.network('https://s-i.huffpost.com/gen/3948866/thumbs/o-PEPE-THE-FROG-570.jpg?3',fit: BoxFit.cover)
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(padding: EdgeInsets.all(3.0)),
+                      Text('FaceBook 친구'),
+                      Padding(padding: EdgeInsets.all(8.0)),
+                      RaisedButton(
+                        child: Text('follow'),
+                        color: Colors.lightBlue,
+                        textColor: Colors.white,
+                        onPressed: (){},
+                      ),
+                      Padding(padding: EdgeInsets.all(8.0)),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        )
+      ),),
+    );
+  }
+}
