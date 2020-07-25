a16d8520cebc63bf586159c4e79fb5bc867f0c16
Lee bosung <dlqh406@gmail.com>
2020-07-26 06:53:23 +0900

    초기 셋팅
    main.dart: 처음앱이 시작될때 rootPage로 연결해줌
    root_page.dart : 로그인 여부를 판단하고, 안되어있다면 로그인페이지로, 되어있다면 Tab_page로 보낼 페이지(추후)
    tab_page.dart : 홈화면으로 설정 할 예정

---
+++ lib/root_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_basic_instacloneapp/tab_page.dart';
+//root: 뿌려주는 페이지니깐 stateless로
+class RootPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TabPage();
+  }
+}
+
---
+++ lib/tab_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TabPage extends StatefulWidget {
+  @override
+  _TabPageState createState() => _TabPageState();
+}
+
+class _TabPageState extends State<TabPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("tab page"),
+    );
+  }
+}
