a72ae4449df2721a544e777b9bdd738a53f6a9a8
Lee bosung <dlqh406@gmail.com>
2020-07-26 07:10:50 +0900

    tab_page.dart: bottomnavigatorBarItems에 index부여하여 버튼 활성화

--- lib/tab_page.dart
+++ lib/tab_page.dart
@@ -6,20 +6,29 @@ class TabPage extends StatefulWidget {
 }
 
 class _TabPageState extends State<TabPage> {
+  int _selectedIndex =  0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
+        onTap: _onItemTapped,
+        currentIndex: _selectedIndex,
         items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("Home")),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.search), title: Text("Search")),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.account_circle), title: Text("Account")),
+          BottomNavigationBarItem(icon: Icon(Icons.search), title: Text("Search")),
+          BottomNavigationBarItem(icon: Icon(Icons.account_circle), title: Text("Account")),
 
         ]
 
         ,),
     );
   }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 }
+
