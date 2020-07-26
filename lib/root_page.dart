928e24b58a01fc599bbc151b785308b9fff6d416
Lee bosung <dlqh406@gmail.com>
2020-07-26 18:56:51 +0900

    login_page.dart : 로그인 페이지 UI  작성
    ✅pubspec에서 외부 라이브러리 설치  : signing_button

--- lib/root_page.dart
+++ lib/root_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basic_instacloneapp/login_page.dart';
 import 'package:flutter_basic_instacloneapp/tab_page.dart';
+
+
 //root: 뿌려주는 페이지니깐 stateless로
 class RootPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return TabPage();
+    return LoginPage();
   }
 }
 
