e4f8971b13f7c262ae734fedbeafc445a99ef1e3
Lee bosung <dlqh406@gmail.com>
2020-09-11 08:32:04 +0900

    빈 커밋

--- lib/root_page.dart
+++ lib/root_page.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_basic_instacloneapp/login_page.dart';
 import 'package:flutter_basic_instacloneapp/tab_page.dart';
 
-
+  
 //root: 뿌려주는 페이지니깐 stateless로
 class RootPage extends StatelessWidget {
   @override
